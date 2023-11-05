165c3106d5f1186e14bad6aad6755ec03a6bead4
md-pranta <juhurenurulhaque@gmail.com>
2023-11-05 20:41:11 +0600

    day5

---
+++ Novmember/day5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getWinner(self, arr: list[int], k: int) -> int:
+        maxNumber = max(arr)
+        current = arr[0]
+        win = 0
+        for i in range(1, len(arr)):
+            if current > arr[i]:
+                win += 1
+            else:
+                current = arr[i]
+                win = 1
+            if win == k or maxNumber == current:
+                return current
