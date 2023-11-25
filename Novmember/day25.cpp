9d91d7407471454ba115505571230eb5cdd693b7
md-pranta <juhurenurulhaque@gmail.com>
2023-11-25 22:39:29 +0600

    day25

---
+++ Novmember/day25.cpp
@@ -0,0 +1,16 @@
+class Solution:
+    def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [nums[0]]
+        for i in range(1,n):
+            prefix.append(prefix[i-1]+nums[i])
+        result = []
+        for i in range(n):
+            left = prefix[i]-nums[i]
+            left = nums[i] * i - left
+
+            right = prefix[n-1]-prefix[i]
+            right -= nums[i] * (n-1-i)
+
+            result.append(left+right)
+        return result
